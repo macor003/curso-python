388fd25723af3d7d87e906ca04826960887c4848
Mario Ortega <mcor63@gmail.com>
2020-10-26 20:51:42 -0500

    Se realiza practicas con funciones

--- conversor.py
+++ conversor.py
@@ -1,3 +1,11 @@
+def conversor(tipo_moneda, valor_dolar):
+    pesos = float(input('Cuantos pesos ' + tipo_moneda + ' tienes?: '))
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print('Tienes $' + dolares + ' Dolares')
+
+
 menu = """
 Bienvenido al conversor de monedas
 
@@ -10,26 +18,10 @@ Elige una opcion (numero): """
 opcion = int(input(menu))
 
 if opcion == 1:
-    pesos = float(input('Cuantos pesos Colombianos tienes?: '))
-    valor_dolar = 3875
-    dolares = pesos/valor_dolar
-    dolares = round(dolares, 2)
-    dolares = str(dolares)
-    print('Tienes $' + dolares + ' Dolares')
+    conversor('Colombianos', 3870)
 elif opcion == 2:
-    pesos = float(input('Cuantos pesos Argentinos tienes?: '))
-    valor_dolar = 65
-    dolares = pesos/valor_dolar
-    dolares = round(dolares, 2)
-    dolares = str(dolares)
-    print('Tienes $' + dolares + ' Dolares')
-
+    conversor('Argentinos', 65)
 elif opcion == 3:
-    pesos = float(input('Cuantos pesos Mexicanos tienes?: '))
-    valor_dolar = 20
-    dolares = pesos/valor_dolar
-    dolares = round(dolares, 2)
-    dolares = str(dolares)
-    print('Tienes $' + dolares + ' Dolares')
+    conversor('Mexicanos', 25)
 else:
     print('Ingresa una opcion correcta por favor')
--- funciones.py
+++ funciones.py
@@ -1,8 +1,28 @@
+#
+# def imprimir_mensaje():
+#     print('Mensaje especial')
+#
+#
+# imprimir_mensaje()
+# imprimir_mensaje()
 
-def imprimir_mensaje():
-    print('Mensaje especial')
+# def mensajes(opcion):
+#     print('Elegiste la opcion ' + opcion)
+#
+#
+# seleccion = int(input('Elige una opcion (1, 2, 3): '))
+# if seleccion == 1:
+#     mensajes(str(seleccion))
+# elif seleccion == 2:
+#     mensajes(str(seleccion))
+# elif seleccion == 3:
+#     mensajes(str(seleccion))
+# else:
+#     print('Escribe una de las opciones')
+
+def suma(a, b):
+    print('Se suman dos numeros')
+    return a + b
 
 
-imprimir_mensaje()
-imprimir_mensaje()
-
+print(suma(1, 4))
