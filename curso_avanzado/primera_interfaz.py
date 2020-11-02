6583eae68a0df7572c157899d3185732303dfb8c
Mario Ortega <mcor63@gmail.com>
2020-11-02 15:47:47 -0500

    Se realiza practica con una interfaz

---
+++ curso_avanzado/primera_interfaz.py
@@ -0,0 +1,15 @@
+import turtle
+
+window = turtle.Screen()
+mario = turtle.Turtle()
+
+mario.forward(100)
+mario.left(90)
+mario.forward(100)
+mario.left(90)
+mario.forward(100)
+mario.left(90)
+mario.forward(100)
+mario.left(90)
+
+turtle.mainloop()
