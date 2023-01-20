b622550fb260b1ba038e28acc817c75a82550fce
Mario Ortega <mcor63@gmail.com>
2023-01-19 21:41:49 -0500

    Challenge 3 (Password Generator) is completed.

--- retos_programacion2023/Reto3.py
+++ retos_programacion2023/Reto3.py
@@ -1,5 +1,14 @@
 import random
 
+# Reto #3: EL GENERADOR DE CONTRASEÑAS
+# Escribe un programa que sea capaz de generar contraseñas de forma aleatoria.
+# Podrás configurar generar contraseñas con los siguientes parámetros:
+# - Longitud: Entre 8 y 16.
+# - Con o sin letras mayúsculas.
+# - Con o sin números.
+# - Con o sin símbolos.
+# (Pudiendo combinar todos estos parámetros entre ellos)
+# 
 
 def generate_password():
     capitals = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
